f90766f2173abfc209781a99bfb23b91776c6fa0
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-03 18:42:14 +0100

    solve lock boxes with comments

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,86 +8,78 @@ def canUnlockAll(boxes):
     """ This method determines if all the boxes can be opened
     """
 
-    # Store opened boxes here, first box is opened
+    # Number of boxes
+    number_of_boxes = len(boxes)
+
+    # Save retrieved keys
     extracted_keys = {0}
-    current_boxes = []
-    unlocked_boxes = []
+
+    # first box is opened retreive the keys
     first_box = boxes[0]
+    for initial_key in first_box:
+        extracted_keys.add(initial_key)
+    # print("first extracted keys = ", extracted_keys)
+    # print("number of keys = ", number_of_boxes)
+ 
+    index = 1
+    # Search for the keys
+    # boxes = [[1], [2], [3], [4], []]
+    # boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    # boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 
-    # 1 iteration
-    # Open first box
-    current_boxes.append(first_box)
+    while index < len(extracted_keys):
+        # print("index = ", index)
+        # print("boxes[{}] = ".format(index), boxes[index])
+        # Open the box and the retrieve keys
+        for key in boxes[index]:
+            # print("key = ", key)
+            if key not in extracted_keys:
+                extracted_keys.add(key)
+                # print("Extracted keys =", extracted_keys)
+        index += 1
 
-    # print("Extracted keys = ", extracted_keys)
-    # print(boxes)
-    # print("opened boxes = ", current_boxes)
+    # print(len(extracted_keys))
+    # print(number_of_boxes)
 
-    # Extract the keys in first box
-    for key in first_box:
-        # save the opened boxes
-        extracted_keys.add(key)
-        current_boxes.append(boxes[key])
+    if len(extracted_keys) == number_of_boxes:
+        return True
+    else:
+        return False
 
-    # print("visited boxes = ", extracted_keys)
-    # print(boxes)
-    # print("opened boxes = ", current_boxes)
+# canUnlockAll([[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]])
 
-    # 2
-    # Open the new boxes using the extracted keys
-    # to get new keys
-    new_keys = {}
-    new_keys = {keys for box in current_boxes for keys in box}
+# canUnlockAll([[1], [2], [3], [4], []])
+    # # 1 iteration
+    # # Open first box
+    # current_boxes.append(first_box)
 
-    # print("new keys = ", new_keys)
+    # index = 0
+    # while index < number_of_boxes:
+    #     # Open the new boxes using the extracted keys
+    #     # to get new keys
+    #     # Extract the keys in new opened boxes
+    #     set_of_keys = boxes[keys]
+    #     new_keys = {keys for box in current_boxes for keys in box}
+    #     for key in new_keys:
+    #         extracted_keys.add(key)
+    #         current_boxes.append(boxes[key])
 
-    # Extract the keys in new opened boxes
-    current_boxes = []
-    for key in new_keys:
-        extracted_keys.add(key)
-        current_boxes.append(boxes[key])
+    #     print("visited boxes = ", extracted_keys)
+    #     print("opened boxes = ", current_boxes)
+    #     print("new keys = ", new_keys)
+    #     new_keys = {}
+    #     current_boxes = []
 
-    # print("visited boxes = ", extracted_keys)
-    # print(boxes)
-    # print("opened boxes = ", current_boxes)
+    #     index += 1
 
-    # 3
-    # Open the new boxes using the extracted keys
-    # to get new keys
-    new_keys = {}
-    new_keys = {keys for box in current_boxes for keys in box}
+    # unlocked_boxes = [boxes[key] for key in extracted_keys]
 
-    # print("new keys = ", new_keys)
-
-    # Extract the keys in new opened boxes
-    current_boxes = []
-    for key in new_keys:
-        extracted_keys.add(key)
-        current_boxes.append(boxes[key])
-
-    # print("visited boxes = ", extracted_keys)
-    # # print(boxes)
-    # print("opened boxes = ", current_boxes)
-
-        # 3
-    # Open the new boxes using the extracted keys
-    # to get new keys
-    new_keys = {}
-    new_keys = {keys for box in current_boxes for keys in box}
-
-    # print("new keys = ", new_keys)
-
-    # Extract the keys in new opened boxes
-    current_boxes = []
-    for key in new_keys:
-        extracted_keys.add(key)
-        current_boxes.append(boxes[key])
-
-    unlocked_boxes = [boxes[key] for key in extracted_keys]
+    # # All boxes are unlocked return true
+    # if unlocked_boxes == boxes:
+    #     return True
+    
 
     # print("unlocked boxes", unlocked_boxes)
     # print("boxes", boxes)
-    if unlocked_boxes == boxes:
-        return True
-
-    return False
-    # last_key = new_keys - extracted_keys
+    # return False
+    # # last_key = new_keys - extracted_keys
