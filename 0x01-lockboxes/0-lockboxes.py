c58fecd40cb7bcf116f6b951f875b0336c5b7a4a
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-03 22:50:10 +0100

    fix bug for egde cases

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,26 +11,25 @@ def canUnlockAll(boxes):
     # Number of boxes
     number_of_boxes = len(boxes)
 
-    # Save retrieved keys
-    extracted_keys = {0}
-
     # first box is opened retreive the keys
     first_box = boxes[0]
-    for initial_key in first_box:
-        extracted_keys.add(initial_key)
+    keys_at_hand = [initial_key for initial_key in first_box if initial_key < number_of_boxes]
 
-    index = 1
+    index = 0
     # Search for the keys
-    while index < len(extracted_keys):
+    while index < len(keys_at_hand):
         # Open the box and the retrieve keys
-        for key in boxes[index]:
+        # print("keys at hand = ", keys_at_hand)
+        # print("boxes[{}] = ".format(keys_at_hand[index]), boxes[keys_at_hand[index]])
+        for key in boxes[keys_at_hand[index]]:
             # if key is retreived twice skip
-            if key not in extracted_keys:
-                extracted_keys.add(key)
+            if key not in keys_at_hand and key < number_of_boxes and key != 0:
+                # print("key = ", key)
+                keys_at_hand.append(key)
         index += 1
 
-    # Check if all boxes have been opened
-    if len(extracted_keys) == number_of_boxes:
+    # Check if all boxes have been opened indexing from 0
+    if len(keys_at_hand) == number_of_boxes - 1:
         return True
     else:
         return False
