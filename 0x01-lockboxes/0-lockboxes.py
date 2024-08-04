4d70ae4924f52139c1b4d8f2b2e2189af9a2111f
Yahaya Azeez Adebayo <azconcept2016@gmail.com>
2024-08-04 09:27:45 +0100

    Remove code replication for only 1 box

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,33 +11,21 @@ def canUnlockAll(boxes):
     # Number of boxes
     number_of_boxes = len(boxes)
 
-    # Since first box is always open only one box is true
-    if number_of_boxes == 1:
-        return True
-
-    # first box is opened retreive the keys
-    # first_box = boxes[0]
-
     # Get initial keys at hand starting from first box
     keys_at_hand = [0]
-    # for initial_key in first_box:
-    #     if initial_key < number_of_boxes:
-    #         keys_at_hand.append(initial_key)
 
+    # Starting to search from 1st key at hand
     index = 0
     # Search for the keys
     while index < len(keys_at_hand):
-        # Open the box and the retrieve keys
+        # Open the box to retrieve the keys
         for key in boxes[keys_at_hand[index]]:
-            # if key is retreived twice skip
+            # skip previously retreived keys & discard unwanted keys
             if key not in keys_at_hand and key < number_of_boxes:
                 keys_at_hand.append(key)
         index += 1
 
-    # Check if all boxes have been opened indexing from 0
-    # print("keys = ", keys_at_hand)
-    # print("total keys = ", len(keys_at_hand))
-    # print("Boxes = ", number_of_boxes)
+    # Check if all boxes have been opened
     if len(keys_at_hand) == number_of_boxes:
         return True
     else:
