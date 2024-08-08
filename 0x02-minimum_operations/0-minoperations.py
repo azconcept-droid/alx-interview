3684066e70d2bd94b21fb6ede294a9b3053d938f
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-08 22:38:25 +0100

    add egde cases for n = 0, 1 and 2

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,12 +2,25 @@
 """
 This module minimum operation
 """
-"""
-H
-copyall paste H
-PASTE HH
-PASTE HHH
-COPYALL PASTE HHHHHH
-PASTE HHHHHHHHH
-"""
-# H HH 
+
+
+def minOperations(n):
+    """ method that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file.
+    Example:
+
+    n = 9
+
+    H => Copy All => Paste => HH => Paste =>HHH =>
+    Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+
+    Number of operations: 6
+    """
+
+    # when n = 0 or 1
+    if n == 0 or n == 1:
+        return 0
+
+    # when n = 2 only two operation
+    if n == 2:
+        return 2
