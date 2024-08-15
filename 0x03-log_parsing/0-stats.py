9fa874ae169b3e861c74129c13e7d124b5a661ab
Yahaya Azeez <azconcept2016@gmail.com>
2024-08-15 12:34:13 +0100

    add some code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,5 +2,18 @@
 """
 Log parsing module
 """
+import sys
 
+# 129.102.207.84 - [2024-08-15 11:17:23.534639] "GET /projects/260 HTTP/1.1" 403 896
+# 103.103.37.244 - [2024-08-15 11:17:23.538151] "GET /projects/260 HTTP/1.1" 403 969
+# 231.231.79.102 - [2024-08-15 11:17:23.657267] "GET /projects/260 HTTP/1.1" 401 145
+# 166.51.143.15 - [2024-08-15 11:17:24.558030] "GET /projects/260 HTTP/1.1" 401 46
 
+count = 0
+for line in sys.stdin:
+
+    if count == 10:
+        break
+    for x in line.split(' '):
+        print(x)
+    count += 1
