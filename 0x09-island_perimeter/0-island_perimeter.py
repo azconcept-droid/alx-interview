f0e52dbf984fb23c89eace7f84f75da0e0e5a001
Yahaya Azeez <azconcept2016@gmail.com>
2024-09-26 15:57:39 +0100

    add some codes

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,3 +2,33 @@
 """
 This module calculate island perimeter
 """
+
+
+def island_perimeter(grid):
+    """Calculate the perimeter of an Island"""
+
+    # Breadth of grid
+    breadth = len(grid[0])
+
+    # length of grid
+    length = len(grid)
+
+    # row index
+    row_index = 0
+
+    # column index
+    col_index = 0
+
+    for row in grid:
+        # iterate through each cell
+        for cell in row:
+            print(cell)
+            if cell == 1:
+                sorround = 4
+                # check if it get to the end of the row
+                if row_index != breadth - 1:
+                    if grid[row_index, col_index + 1] == 1:
+                        sorround -= 1
+                    if grid[row_index + 1, col_index] == 1:
+                        sorround -= 1
+                
--- 0x09-island_perimeter/README.md
+++ 0x09-island_perimeter/README.md
@@ -1,2 +1,4 @@
 #  0x09. Island Perimeter
 ``Algorithm`` ``Python``
+
+- [Mock interview](https://www.youtube.com/watch?v=fFgEM6CMQc4)
