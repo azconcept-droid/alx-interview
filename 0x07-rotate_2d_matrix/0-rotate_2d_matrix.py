780dc8ad3681da9cd1fb4e3a49d846d57c90a476
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-23 18:35:01 +0100

    flip matrix 90 degree clockwise

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,5 +4,20 @@
 
 
 def rotate_2d_matrix(matrix):
-  """ This function rotate 2D matrix in-place"""
-  pass
+    """ This function rotate 2D matrix in-place"""
+
+    # matrix dimension
+    n = len(matrix)
+
+    # Initialize temporary matrix of n x n with zeros
+    tmp = [[0] * n for _ in range(n)]
+
+    # Flip the matrix clockwise
+    for i in range(n):
+        for j in range(n):
+            tmp[j][n - i - 1] = matrix[i][j]
+
+    # update the matrix in-place
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = tmp[i][j]
