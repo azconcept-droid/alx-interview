10dc8d7dc540a0159b6c3de9571e8bc6e6471b22
Yahaya Azeez <azconcept2016@gmail.com>
2024-11-28 18:17:40 +0100

    change coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
+Given a pile of coins of different values,
+determine the fewest number of coins needed to meet a given amount total.
 
     Prototype: def makeChange(coins, total)
     Return: fewest number of coins needed to meet total
@@ -8,7 +9,8 @@ Given a pile of coins of different values, determine the fewest number of coins 
         If total cannot be met by any number of coins you have, return -1
     coins is a list of the values of the coins in your possession
     The value of a coin will always be an integer greater than 0
-    You can assume you have an infinite number of each denomination of coin in the list
+    You can assume you have an infinite number of each
+    denomination of coin in the list
     Your solution’s runtime will be evaluated in this task
 
 """
@@ -19,18 +21,22 @@ def makeChange(coins, total):
     the given total amount
     """
     n = len(coins)
+    flag = 0
     if total <= 0:
         return 0
     if n == 0:
         return -1
-    db = [[0 for x in range(total+1)] for x in range(n+1)]
+    # sort coins to highest denomination first
+    coins.sort(reverse=True)
 
-    for i in range(n+1):
-        db[i][0] = 1
-    for i in range(1, n + 1):
-        for j in range(1, total + 1):
-            db[i][j] = db[i - 1][j]
-            if j >= coins[i - 1]:
-                db[i][j] += db[i][j - coins[i - 1]]
+    # save used denomination
+    denomination = []
 
-    return db[n][total]
+    for i in range(n - 1):
+        while (total >= coins[i]):
+            total = total - coins[i]
+            denomination.append(coins[i])
+            if total == 0:
+                return len(denomination)
+
+    return -1
